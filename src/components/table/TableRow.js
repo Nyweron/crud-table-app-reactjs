9698273734353d8250a6cda50b7b35b80d900e5c
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2018-09-03 20:13:26 +0200

    Display dynamically list almost works...

--- src/components/table/TableRow.js
+++ src/components/table/TableRow.js
@@ -15,18 +15,64 @@ export const TableRow = props => {
 
   let rowsToReturn = [];
   var keys=props.keys;
+  var key = ["id", "firstName", "age", "lastName", "isActive"];
+  var rows = [
+    {
+      "id": 1,
+      "firstName": "Homer",
+      "lastName": "Simpson",
+      "age": 45,
+      "isActive": true
+    },
+    {
+      "id": 2,
+      "firstName": "Merge",
+      "lastName": "Simpson",
+      "age": 42,
+      "isActive": true
+    },
+    {
+      "id": 3,
+      "firstName": "Bart",
+      "lastName": "Simpson",
+      "age": 15,
+      "isActive": true
+    },
+    {
+      "id": 4,
+      "firstName": "Lisa",
+      "lastName": "Simpson",
+      "age": 12,
+      "isActive": false
+    },
+    {
+      "id": 5,
+      "firstName": "Maggie",
+      "lastName": "Simpson",
+      "age": 1,
+      "isActive": true
+    }
+  ];
   console.log("keys",keys)
   console.log("props.rows",props.rows)
   console.log("props.keys",props.keys)
 
-  for(let i = 0; i < 5; i++){
-    for(let j = 0; j < 5; j++){
-      console.log("test",props.rows[i][keys[j]]);
-     // rowsToReturn.push(<th>{props.rows[i][keys[j]]}</th>)
+  const keyLength = props.keys.length;
+
+  for(let i = 0; i < keyLength; i++){
+    rowsToReturn.push('<td>'); //<- this part should have <td>
+    for(let j = 0; j < keyLength; j++){
+      console.log("test",rows[i][key[j]]);
+      rowsToReturn.push(<th>{rows[i][key[j]].toString()}</th>)
+      if(j==4){
+        rowsToReturn.push(`\n`)
+        rowsToReturn.push('</tr>') //<- this part should cloase </td>
+      }
     }
   }
-
-  return <tr>{rowsToReturn}</tr>
+console.log("rowsToReturn", rowsToReturn)
+ // return <tr>{rowsToReturn}</tr>
+  return rowsToReturn
 };
 
 TableRow.propTypes = {
