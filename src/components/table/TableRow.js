c30785c40c073c7f0a2930c91d5bc07f54c8a61e
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2018-09-03 21:02:48 +0200

    Refactor TableRow

--- src/components/table/TableRow.js
+++ src/components/table/TableRow.js
@@ -2,74 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const TableRow = props => {
-  // return (
-  //   <tr>
-  //     <th>{props.id}</th>
-  //     <th>{props.firstName}</th>
-  //     <th>{props.lastName}</th>
-  //     <th>{props.age}</th>
-  //     {/* <th>{props.isActive.toString()}</th> */}
+  let rowsToReturn = props.rows.map(function(row) {
+    return(
+      <tr>
+        {props.keys.map(function (key){
+          return <td>{row[key].toString()}</td>;
+        })}
+      </tr>
+  );});
 
-  //   </tr>
-  // );
-
-
-  let rowsToReturn = [];
-  var keys=props.keys;
-  var key = ["id", "firstName", "age", "lastName", "isActive"];
-  var rows = [
-    {
-      "id": 1,
-      "firstName": "Homer",
-      "lastName": "Simpson",
-      "age": 45,
-      "isActive": true
-    },
-    {
-      "id": 2,
-      "firstName": "Merge",
-      "lastName": "Simpson",
-      "age": 42,
-      "isActive": true
-    },
-    {
-      "id": 3,
-      "firstName": "Bart",
-      "lastName": "Simpson",
-      "age": 15,
-      "isActive": true
-    },
-    {
-      "id": 4,
-      "firstName": "Lisa",
-      "lastName": "Simpson",
-      "age": 12,
-      "isActive": false
-    },
-    {
-      "id": 5,
-      "firstName": "Maggie",
-      "lastName": "Simpson",
-      "age": 1,
-      "isActive": true
-    }
-  ];
-
-  const keyLength = props.keys.length;
-
-  var x = rows.map(function(row) {
-        return(
-          <tr>
-            {keys.map(function (column){
-              return <td>{row[column].toString()}</td>;
-            })}
-          </tr>
-  )});
-
-  console.log("x",x)
-
-  return x
-
+  return rowsToReturn
 };
 
 TableRow.propTypes = {
