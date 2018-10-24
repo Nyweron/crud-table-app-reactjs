06e2d13e7af84a7ce70370bcd51f458e16d3c0fb
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2018-10-24 22:15:05 +0200

    task :lipstick: prettier formarer from char syntax to string

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 //import {Router} from './components/router'
 
 ReactDOM.render(
   // <Router>
-    <App />,
-    // </Router>,
-  document.getElementById('root')
+  <App />,
+  // </Router>,
+  document.getElementById("root")
 );
 registerServiceWorker();
