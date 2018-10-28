84f408891784589431804123be10f528c80ef7ce
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2018-10-28 14:47:17 +0100

    task :sparkles: Add test to removeRowById

--- src/lib/personHelpers.test.js
+++ src/lib/personHelpers.test.js
@@ -1,4 +1,4 @@
-import { findById } from "./personHelpers";
+import { removeRowById, findById } from "./personHelpers";
 
 test("findById should find object by id from list", () => {
   const startPersons = [
@@ -30,5 +30,56 @@ test("findById should find object by id from list", () => {
   };
 
   const result = findById(startPersons, id);
+  expect(result).toEqual(expected);
+});
+
+test("removeRowById should remove one element from array by id", () => {
+  const startPersons = [
+    {
+      id: 1,
+      firstName: "Tom",
+      lastName: "Cat",
+      age: 10,
+      isActive: true,
+      hobby: "Bike"
+    },
+    {
+      id: 2,
+      firstName: "Jerry",
+      lastName: "Mouse",
+      age: 11,
+      isActive: true,
+      hobby: "Sleep"
+    },
+    {
+      id: 3,
+      firstName: "Duck",
+      lastName: "Donald Duck",
+      age: 12,
+      isActive: false,
+      hobby: "Play tennis"
+    }
+  ];
+  const id = 2;
+  const expected = [
+    {
+      id: 1,
+      firstName: "Tom",
+      lastName: "Cat",
+      age: 10,
+      isActive: true,
+      hobby: "Bike"
+    },
+    {
+      id: 3,
+      firstName: "Duck",
+      lastName: "Donald Duck",
+      age: 12,
+      isActive: false,
+      hobby: "Play tennis"
+    }
+  ];
+
+  const result = removeRowById(startPersons, id);
   expect(result).toEqual(expected);
 });
